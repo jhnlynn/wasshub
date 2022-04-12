1bfe476170c6b431812b2481febb3dfdaa62a09e
jhnlynn <jl12183@nyu.edu>
2022-04-12 00:42:54 -0400

    CustomError as abstract class instead

--- auth/src/errors/database-connection-error.ts
+++ auth/src/errors/database-connection-error.ts
@@ -1,12 +1,13 @@
 import { ValidationError } from "express-validator"; 
+import { CustomError } from "./custom-error";
 
 // database specified errors
-export class DatabaseConnectionError extends Error {
+export class DatabaseConnectionError extends CustomError {
     statusCode = 500;
     reason = 'Error connecting to DB';
     
     constructor(public errors: ValidationError[]) {
-        super();
+        super('Error connecting to DB');
         
         Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
     } 
--- auth/src/errors/request-validation-error.ts
+++ auth/src/errors/request-validation-error.ts
@@ -1,23 +1,17 @@
 import { ValidationError } from "express-validator"; 
+import { CustomError } from "./custom-error";
 
-interface CustomError {
-    statusCode: number,
-    serializaErrors(): {
-        message: string;
-        field?: string;
-    }[]
-}
 
-export class RequestValidationError extends Error implements CustomError {
+export class RequestValidationError extends CustomError {
     statusCode = 400;
 
     constructor(public errors: ValidationError[]) {
-        super();
+        super('Invalid request parameters');
         
         Object.setPrototypeOf(this, RequestValidationError.prototype);
     } 
 
-    serializaErrors() {
+    serializeErrors() {
         return this.errors.map(err => {
             return { message: err.msg, field: err.param }
         })
