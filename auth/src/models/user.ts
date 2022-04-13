b1192835f96208e2398cbdbc2e33bf7e3a3112e2
jhnlynn <jl12183@nyu.edu>
2022-04-13 18:06:42 -0400

    user signup process with password services

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { Password } from '../services/password';
 
 // An interface describes the properties of 
 // required to create a new user, 
@@ -33,6 +34,14 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     }
+});
+
+userSchema.pre('save', async function(done) {
+    if (this.isModified('password')) {
+      const hashed = await Password.toHash(this.get('password'));
+      this.set('password', hashed);
+    }
+    done();
 });
 
 // every time create a new user, use this function
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
 import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { BadRequestError } from '../errors/bad-request-error';
+import { User } from '../models/user';
 
 const router = express.Router();
 
@@ -22,11 +23,18 @@ async (req: Request, res: Response) => {
         throw new RequestValidationError(errors.array());    
     }
 
-    console.log('Creating a user...');
-    throw new DatabaseConnectionError(errors.array());
+    const { email, password } = req.body;
 
-    res.send({});
-    
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+        throw new BadRequestError('Email in use');
+    }
+
+    const user = User.build({ email, password });
+    await user.save();
+
+    res.status(201).send(user);
 });
 
 export { router as signupRouter };
