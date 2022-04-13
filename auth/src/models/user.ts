054ac6d8078156952dfd4d9ce2712b7e5615de53
jhnlynn <jl12183@nyu.edu>
2022-04-13 17:47:28 -0400

    complete user model,  and resolve issues of 'ts working with mongoose'

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,6 +1,28 @@
 import mongoose from 'mongoose';
 
+// An interface describes the properties of 
+// required to create a new user, 
+// to prevent issue #1 of 'using ts with mongoose': 
+//      ts ==> mongoose
+interface userAttrs {
+    email: string;
+    password: string;
+}
 
+// An interface describes the properties 
+// that a User Model has
+// to prevent issue #2 of 'using ts with mongoose':
+//      mongoose ==> ts
+interface UserModel extends mongoose.Model<UserDoc> {
+    build(attrs: userAttrs): UserDoc;
+}
+
+// An interface describes the properties 
+// that a User Document (of MongoDB) has
+interface UserDoc extends mongoose.Document {
+    email: string,
+    password: string
+}
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -13,6 +35,12 @@ const userSchema = new mongoose.Schema({
     }
 });
 
-const User = mongoose.model('User', userSchema);
+// every time create a new user, use this function
+// instead of the above one
+userSchema.statics.build = (attrs: userAttrs) => {
+    return new User(attrs);
+};
+
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
 
 export { User };
