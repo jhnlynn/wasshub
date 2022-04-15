d2ecd0aba5cde0c17174a2d5c4bdf44867b58e34
jhnlynn <jl12183@nyu.edu>
2022-04-15 16:09:36 -0400

    fix bugs in product images

--- product/src/index.ts
+++ product/src/index.ts
@@ -12,13 +12,13 @@ const start = async () => {
 
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log('Connected to MongoDb');
+    console.log('Product: Connected to MongoDb');
   } catch (err) {
     console.error(err);
   }
 
   app.listen(3000, () => {
-    console.log('Listening on port 3000!!!!!!!!');
+    console.log('Product: Listening on port 3000!!!!!!!!');
   });
 };
 
